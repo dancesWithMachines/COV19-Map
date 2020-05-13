1345e1e357c82f6b27ec85283a636b9b27327392
Timax <timax2@wp.pl>
2020-05-13 16:50:17 +0200

    Updates + screenshots

--- COV19Map/app/src/main/java/com/example/cov19_map/MapsActivity.java
+++ COV19Map/app/src/main/java/com/example/cov19_map/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.cov19_map;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,6 +9,8 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -15,6 +18,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
@@ -38,6 +42,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -47,6 +53,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.UrlTileProvider;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -57,16 +68,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_CODE =911;
     private GoogleMap mMap;
+    Location myLocation;
+    LatLng userLocation;
+    List<AuthUI.IdpConfig> providers;
     private String jsonUrl = "https://disease.sh/v2/countries?yesterday=false";
     LocationManager locationManager;
     LocationListener locationListener;
+    MenuItem userData;
 
     //On create
     @Override
@@ -78,7 +95,58 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //lista metod logowania
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(), //Email
+                new AuthUI.IdpConfig.PhoneBuilder().build(), //Phone
+                new AuthUI.IdpConfig.GoogleBuilder().build() //Google
+        );
+
+        showSignInOptions();
     }
+
+    //Okno metody logowania
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).setTheme(R.style.AppTheme).setLogo(R.drawable.cov19map).build(),
+                REQUEST_CODE
+        );
+    }
+
+    //Obługa responsa
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            IdpResponse response =IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                userData.setTitle(user.getDisplayName());
+                if (mMap != null){
+                    GetCountries getCountries = new GetCountries();
+                    getCountries.execute(jsonUrl);
+                }
+            } else {
+                Toast.makeText(this, ""+response.getError().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    //Obsługa okna dialogowego
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    //Yes button clicked
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //No button clicked
+                    break;
+            }
+        }
+    };
 
     //On map ready
     @Override
@@ -86,8 +154,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
         modifySnippet();
 
-        GetCountries getCountries = new GetCountries();
-        getCountries.execute(jsonUrl);
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }else{
+            if(!mMap.isMyLocationEnabled())
+                mMap.setMyLocationEnabled(true);
+
+            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (myLocation == null) {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+                String provider = lm.getBestProvider(criteria, true);
+                myLocation = lm.getLastKnownLocation(provider);
+            }
+
+            if(myLocation!=null){
+                userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 4), 1500, null);
+            }
+        }
     }
 
     //modyfikuje domyślny snippet map google
@@ -127,6 +214,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
+        userData = menu.findItem(R.id.userName);
         return true;
     }
 
@@ -134,11 +222,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.showMe:
-                Toast.makeText(this, "Znajdź mnie", Toast.LENGTH_SHORT).show();
-                return true;
             case R.id.logOut:
                 Toast.makeText(this, "Log out", Toast.LENGTH_SHORT).show();
+                AuthUI.getInstance().signOut(MapsActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        showSignInOptions();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MapsActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -192,7 +288,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                     DownloadImage downloadImage = new DownloadImage();
 
-                    //!UWAGA!
+                    //88        88  I8,        8        ,8I    db         ,ad8888ba,         db
+                    //88        88  `8b       d8b       d8'   d88b       d8"'    `"8b       d88b
+                    //88        88   "8,     ,8"8,     ,8"   d8'`8b     d8'                d8'`8b
+                    //88        88    Y8     8P Y8     8P   d8'  `8b    88                d8'  `8b
+                    //88        88    `8b   d8' `8b   d8'  d8YaaaaY8b   88      88888    d8YaaaaY8b
+                    //88        88     `8a a8'   `8a a8'  d8""""""""8b  Y8,        88   d8""""""""8b
+                    //Y8a.    .a8P      `8a8'     `8a8'  d8'        `8b  Y8a.    .a88  d8'        `8b
+                    //`"Y8888Y"'        `8'       `8'  d8'          `8b  `"Y88888P"  d8'          `8b
                     //Jeżeli chcesz by dane ładowały się szybciej zakomentuj wskazane linie!!
                     Bitmap flaga; // <--
                     flaga = downloadImage.execute(countryInfo.getString("flag")).get(); // <--
